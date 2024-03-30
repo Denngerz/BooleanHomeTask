8cdbfc47edb57df1a51d0bc1b8311e1c151c60a7
Denngerz <denngerz@gmail.com>
2024-03-30 06:39:41 +0200

    redone all of them

--- BooleanHomeWork/BooleanHomeWork/Boolean.cpp
+++ BooleanHomeWork/BooleanHomeWork/Boolean.cpp
@@ -1,169 +1,131 @@
 #include "Boolean.h"
 #include <iostream>
-#include <cmath>
 
-void Boolean::Boolean1(const float a) {
-	bool answer = (a > 0);
-
-	std::cout << answer << std::endl;
+bool Boolean::Boolean1(const float a) {
+	return a > 0;
 }
 
-void Boolean::Boolean2(const int a) {
+bool Boolean::Boolean2(const int a) {
+	return a % 2 == 1;
+}
+
+bool Boolean::Boolean5(const int a, const int b) {
+	return  a >= 0 || b < -2;
+}
+
+bool Boolean::Boolean6(const int a, const int b, const int c) {
+	return  (a < b && b < c);
+}
+ 
+bool Boolean::Boolean7(const int a, const int b, const int c) {
+	return b > a && c > b || b < a && c < b;
+}
+
+bool Boolean::Boolean8(const int a, const int b) {
+	return (a * b % 2);
+}
+
+bool Boolean::Boolean11(const int a, const int b) {
+	return (a - b) % 2;
+}
+
+bool Boolean::Boolean12(const int a, const int b, const int c) {
+	return (a > 0) + (b > 0) + (c > 0) == 3;
+ }
+
+bool Boolean::Boolean13(const int a, const int b, const int c) {
+	return (a > 0) + (b > 0) + (c > 0) >= 1;
+}
+
+bool Boolean::Boolean14(const int a, const int b, const int c) {
+	return (a > 0) + (b > 0) + (c > 0) == 1;
+}
+
+bool Boolean::Boolean17(const int a) {
+	bool is_three = a >= 100 && a < 1000;
 	bool is_odd = (a % 2 == 1);
 
-	std::cout << is_odd << std::endl;
+	return is_three && is_odd;
 }
 
-void Boolean::Boolean5(const int a, const int b) {
-	bool is_true = (a >= 0 || b < -2);
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean19(const int a, const int b, const int c) {
+	return (a == -c) || (b == -c) || (b == -a);
 }
 
-void Boolean::Boolean6(const int a, const int b, const int c) {
-	bool is_true = (a < b && b < c);
-
-	std::cout << is_true << std::endl;
-}
-
-void Boolean::Boolean8(const int a, const int b) {
-	bool is_true = (a % 2 == 1 && b % 2 == 1);
-
-	std::cout << is_true << std::endl;
-}
-
-void Boolean::Boolean11(const int a, const int b) {
-	bool is_true = (b % 2 == a % 2);
-
-	std::cout << is_true << std::endl;
-}
-
-void Boolean::Boolean14(const int a, const int b, const int c) {
-	bool is_true = ( ((a > 0) + (b > 0) + (c > 0)) == 1);
-
-	std::cout << is_true << std::endl;
-}
-
-void Boolean::Boolean17(const int a) {
-	bool is_three = (a / 100 > 0);
-	bool is_odd = (a % 2 == 1);
-
-	bool is_true = (is_three && is_odd);
-
-	std::cout << is_true << std::endl;
-}
-
-void Boolean::Boolean19(const int a, const int b, const int c) {
-	bool is_true = ((a == -c) || (b == -c) || (b == -a));
-
-	std::cout << is_true << std::endl;
-}
-
-void Boolean::Boolean20(const int a) {
+bool Boolean::Boolean20(const int a) {
 	int first_digit = a / 100;
 	int second_digit = (a % 100) / 10;
 	int third_digit = a % 10;
 
 	bool is_true = (first_digit != second_digit && second_digit != third_digit && third_digit != first_digit);
 
-	std::cout << is_true << std::endl;
+	return is_true;
 }
 
-void Boolean::Boolean22(const int a) {
+bool Boolean::Boolean22(const int a) {
 	int first_digit = a / 100;
 	int second_digit = (a % 100) / 10;
 	int third_digit = a % 10;
 
-	bool is_true = ((second_digit - first_digit) ==  (third_digit - second_digit) || (second_digit - third_digit) == (first_digit - second_digit));
-
-	std::cout << is_true << std::endl;
+	return (second_digit - first_digit) == (third_digit - second_digit) || (second_digit - third_digit) == (first_digit - second_digit);
 }
 
-void Boolean::Boolean24(const int a, const int b, const int c) {
-	float discriminant = b * b - 4 * a * c;
-
-	bool is_true = discriminant >= 0;
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean24(const int a, const int b, const int c) {
+	return b * b - 4 * a * c >= 0;
 }
 
-
-void Boolean::Boolean26(const int x, const int y) {
-	bool is_true = (x > 0 && y < 0);
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean26(const int x, const int y) {
+	return x > 0 && y < 0;
 }
 
-void Boolean::Boolean28(const int x, const int y) {
-	bool is_true = ((x > 0 && y > 0)  || (x < 0 && y < 0));
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean28(const int x, const int y) {
+	return (x > 0 && y > 0) || (x < 0 && y < 0);
 }
 
-void Boolean::Boolean29(const int x, const int y, const int x1, const int y1, const int x2, const int y2) {
-	bool is_true = ((x < x1) && (x > x2) && (y > y2) && (y < y1));
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean29(const int x, const int y, const int x1, const int y1, const int x2, const int y2) {
+	return (x < x1) && (x > x2) && (y > y2) && (y < y1);
 }
 
-void Boolean::Boolean32(const int a, const int b, const int c) {
+bool Boolean::Boolean32(const int a, const int b, const int c) {
 	const int a_sq = pow(a, 2);
 	const int b_sq = pow(b, 2);
 	const int c_sq = pow(c, 2);
 
-	bool is_true = ((a_sq + b_sq == c_sq) || (b_sq + c_sq == a_sq) || (a_sq + c_sq == b_sq));
-
-	std::cout << is_true << std::endl;
+	return (a_sq + b_sq == c_sq) || (b_sq + c_sq == a_sq) || (a_sq + c_sq == b_sq);
 }
 
-void Boolean::Boolean33(const int a, const int b, const int c) {
-	bool is_true = ((a + b > c)|| (a + c > b) || (b + c > a));
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean33(const int a, const int b, const int c) {
+	return (a + b > c) || (a + c > b) || (b + c > a);
 }
 
-void Boolean::Boolean34(const int x, const int y) {
-	bool is_true = (((x % 2 == 1) && (y % 2 == 0)) || ((x % 2 == 0) && (y % 2 == 1)));
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean34(const int x, const int y) {
+	return (x % 2 == 1) && (y % 2 == 0) || (x % 2 == 0) && (y % 2 == 1);
 }
 
-void Boolean::Boolean35(const int x1, const int y1, const int x2, const int y2) {
+bool Boolean::Boolean35(const int x1, const int y1, const int x2, const int y2) {
 	bool is_white_1 = (((x1 % 2 == 1) && (y1 % 2 == 0)) || ((x1 % 2 == 0) && (y1 % 2 == 1)));
 	bool is_white_2 = (((x2 % 2 == 1) && (y2 % 2 == 0)) || ((x2 % 2 == 0) && (y2 % 2 == 1)));
 
-	bool is_true = is_white_1 == is_white_2;
-
-	std::cout << is_true << std::endl;
+	return is_white_1 == is_white_2;
 }
 
-void Boolean::Boolean36(const int x1, const int y1, const int x2, const int y2) {
-	bool is_true = (x1 == x2 || y1 == y2);
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean36(const int x1, const int y1, const int x2, const int y2) {
+	return  (x1 == x2 || y1 == y2);
 }
 
-void Boolean::Boolean37(const int x1, const int y1, const int x2, const int y2) {
-	bool is_true = ((fabs(x2 - x1) == 1 && fabs(y2 - y1) <= 1) || (fabs(y2 - y1) == 1 && fabs(x2 - x1) <= 1) || (fabs(y2 - y1) == 1 && fabs(x2 - x1) == 1));
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean37(const int x1, const int y1, const int x2, const int y2) {
+	return (abs(x2 - x1) == 1 && abs(y2 - y1) <= 1) || (abs(y2 - y1) == 1 && abs(x2 - x1) <= 1);
 }
 
-void Boolean::Boolean38(const int x1, const int y1, const int x2, const int y2) {
-	bool is_true = ((x2 - x1) == (y2 - y1));
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean38(const int x1, const int y1, const int x2, const int y2) {
+	return abs(x2 - x1) == abs(y2 - y1);
 }
 
-void Boolean::Boolean39(const int x1, const int y1, const int x2, const int y2) {
-	bool is_true = (x1 == x2) || (y1 == y2) || (x2 - x1) == (y2 - y1);
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean39(const int x1, const int y1, const int x2, const int y2) {
+	return (x1 == x2) || (y1 == y2) || (x2 - x1) == (y2 - y1);
 }
 
-void Boolean::Boolean40(const int x1, const int y1, const int x2, const int y2) {
-	bool is_true = ((fabs(x2 - x1) == 1 && fabs(y2 - y1) == 2) || (fabs(x2 - x1) == 2 && fabs(y2 - y1) == 1));
-
-	std::cout << is_true << std::endl;
+bool Boolean::Boolean40(const int x1, const int y1, const int x2, const int y2) {
+	return (abs(x2 - x1) == 1 && abs(y2 - y1) == 2) || (abs(x2 - x1) == 2 && abs(y2 - y1) == 1);
 }
 
--- BooleanHomeWork/BooleanHomeWork/BooleanHomeWork.cpp
+++ BooleanHomeWork/BooleanHomeWork/BooleanHomeWork.cpp
@@ -4,6 +4,7 @@
 int main()
 {
 	Boolean task;
-	task.Boolean40(3,3,2,1);
+	bool answer = task.Boolean7(6, 1, 6);
+	std::cout << answer;
 }
 
